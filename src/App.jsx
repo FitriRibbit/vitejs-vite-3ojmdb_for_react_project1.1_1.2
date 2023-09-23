a872e911f4f477a4b1dc084c51ca81018daa9871
FitriRibbit <91612800+FitriRibbit@users.noreply.github.com>
2023-09-23 08:48:04 -0700

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -41,20 +41,29 @@ const App = () => {
     setBad(newValue);
   };
 
-  return (
-    <div>
-      <h1> give feedback </h1>
-      <Button handleClick={() => setToGood(good + 1)} text="good" /> <nbsp />
-      <Button
-        handleClick={() => setToNeutral(neutral + 1)}
-        text="neutral"
-      />{' '}
-      <nbsp />
-      <Button handleClick={() => setToBad(bad + 1)} text="bad" />
-      <h1> statistics </h1>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
-    </div>
-  );
+  if ( good == 0 && neutral == 0 && bad == 0) {
+    return (
+      <div>
+        <h1> give feedback </h1>
+        <Button handleClick={() => setToGood(good + 1)} text="good" /> <nbsp />
+        <Button handleClick={() => setToNeutral(neutral + 1)} text="neutral"/> <nbsp />
+        <Button handleClick={() => setToBad(bad + 1)} text="bad" />
+        <h1> statistics </h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1> give feedback </h1>
+        <Button handleClick={() => setToGood(good + 1)} text="good" /> <nbsp />
+        <Button handleClick={() => setToNeutral(neutral + 1)} text="neutral" /> <nbsp />
+        <Button handleClick={() => setToBad(bad + 1)} text="bad" />
+        <h1> statistics </h1>
+        <Statistics good={good} neutral={neutral} bad={bad}/>
+      </div>
+    );
+  };
 };
 
 export default App;
