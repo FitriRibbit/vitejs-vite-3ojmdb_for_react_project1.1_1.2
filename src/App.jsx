bbe14fbe4ce8456de159a825c18b236d50d86b57
FitriRibbit <91612800+FitriRibbit@users.noreply.github.com>
2023-09-24 09:28:39 -0700

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,38 @@
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  );
-};
+import { useState } from 'react';
 
-const Part = (props) => {
-  return (
-    <div>
-      <p>
-        {props.pt1} {props.ex1}
-      </p>
-      <p>
-        {props.pt2} {props.ex2}
-      </p>
-      <p>
-        {props.pt3} {props.ex3}
-      </p>
-    </div>
-  );
-};
+const Display = (props) => (
+  <div>
+    {props.text1} {props.selected} {props.text2}
+  </div>
+);
 
-const Content = (props) => {
-  return (
-    <div>
-      <Part pt1={props.part1} ex1={props.exercises1} />
-      <Part pt2={props.part2} ex2={props.exercises2} />
-      <Part pt3={props.part3} ex3={props.exercises3} />
-    </div>
-  );
-};
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>
-        Number of exercises{' '}
-        {props.exercises1 + props.exercises2 + props.exercises3}
-      </p>
-    </div>
-  );
-};
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.',
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const random = anecdotes[Math.floor(Math.random() * anecdotes.length)];
+
   return (
     <div>
-      <Header course={course.name} />
-      <Content
-        part1={course.parts[0].name}
-        exercises1={course.parts[0].exercises}
-      />
-      <Content
-        part2={course.parts[1].name}
-        exercises2={course.parts[1].exercises}
-      />
-      <Content
-        part3={course.parts[2].name}
-        exercises3={course.parts[2].exercises}
-      />
-      <Total
-        exercises1={course.parts[0].exercises}
-        exercises2={course.parts[1].exercises}
-        exercises3={course.parts[2].exercises}
-      />
+      <Display selected={random} />
+      <Display text1="has" selected="" text2="votes" />
+      <Button handleClick={() => setSelected(selected + 1)} text="vote" />{' '}
+      <nbsp />
+      <Button
+        handleClick={() => setSelected(selected + 1)} text="next anecdote" />
     </div>
   );
 };
